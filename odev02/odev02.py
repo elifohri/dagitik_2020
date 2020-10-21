4801ca1a30cfdcdfe69ee5d79625cd13a60ff29c
elifohri <37309266+elifohri@users.noreply.github.com>
2020-10-21 21:55:24 +0300

    Update odev02.py

--- odev02/odev02.py
+++ odev02/odev02.py
@@ -1,1 +1,20 @@
+import sys
 
+s1 = sys.argv[1].strip()
+s2 = sys.argv[2].strip()
+
+myDict = {}
+myList = []
+fid = open("airlines.txt","r")
+
+for line in fid:
+    splittedLine = line.strip().split(",")
+    myList = splittedLine[1:]
+    myDict[splittedLine[0]] = myList
+
+if s2 in myDict[s1] :
+    print("Millerinizi kullanabilirsiniz!")
+else :
+    print("Millerinizi bu havayolunda kullanamazsınız!")   
+
+fid.close()
