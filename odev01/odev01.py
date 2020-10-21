b938b491ac4c360d426dd8a038ec29933a732bc7
elifohri <37309266+elifohri@users.noreply.github.com>
2020-10-21 21:09:40 +0300

    Update odev01.py

--- odev01/odev01.py
+++ odev01/odev01.py
@@ -1,39 +1,34 @@
 import sys
 
 s1 = int(sys.argv[1])
-
 dict = {}
 
-def control(id) :
-    if id in dict :
-        return true
-    else : 
-        return false
-
 while s1 > 0 :
-    text = input("Lütfen birer boşluk bırakarak sırayla numara, ad, soyad ve yaş bilginizi giriniz:").split()
-    
-    s2 = len(text)
-    if control(text[0]) == true :
 
-        if s2 == 4 :
-            no = int(text[0])
-            ad = text[1]
-            soyad = text[2]
-            yas = int(text[3])
-        elif s2 > 4 :
+    text = input("Lütfen birer boşluk bırakarak sırayla numara, ad, soyad ve yaş bilginizi giriniz:").split()
+
+    try:
+        test = int(text[0])
+    except ValueError:
+        print("Numara olarak bir sayı giriniz!")
+    else:
+        try:
+            test2 = int(text[-1])
+        except ValueError:
+            print("Yas olarak bir sayı giriniz!")
+        else: 
             no = int(text[0])
             ad = text[1:-2]
             soyad = text[-2]
-            yas = int(text[-1])        
+            yas = int(text[-1])
 
-        t1 = (ad,soyad,yas) 
-        dict[no] = t1
-        s1 -= 1
+            if no in dict :    
+                print("Girdiginiz numara sözlükte bulunmakta!")        
 
-    else :
-        print("Girdiginiz numara sözlükte bulunmakta!")   
-
+            else:
+                t1 = (ad,soyad,yas)
+                dict[no] = t1
+                s1 -= 1  
 
 dict={key:dict[key] for key in sorted(dict.keys())}
 print(dict)
